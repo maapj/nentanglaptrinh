f8d2130540f36bd8841500dc0815a35f5505cfa1
maapjgit <tranquanghoang.hstranphu@gmail.com>
2022-03-15 20:19:21 +0700

    thuc hanh bien kieu du lieu

--- Day4 bien kieudl/bai thuc hanh/baithuchanh4/main.js
+++ Day4 bien kieudl/bai thuc hanh/baithuchanh4/main.js
@@ -1,12 +1,12 @@
-let inputWidth
-let inputHeight
+let inputWidth;
+let inputHeight;
 
-inputWidth = prompt("Enter the width")
-inputHeight = prompt("Enter the height")
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
 
-let width = parseInt(inputWidth)
-let height = parseInt(inputHeight)
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
 
 
-let area = width * height
-document.write("The area is: " + area)
+let area = width * height;
+document.write("The area is: " + area);
